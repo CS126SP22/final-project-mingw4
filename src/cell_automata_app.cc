26e01820bef3717d4710463107fa5cac5961887d
mingw4 <mingw4@illinois.edu>
2022-04-19 13:45:39 -0500

    Implementations for cc files completed, and CMakelist updated.

--- src/cell_automata_app.cc
+++ src/cell_automata_app.cc
@@ -1,84 +1,216 @@
-#include <cell_automata_visualizer.h>
 #include <cell_automata_app.h>
 
 namespace cellautomata {
 
 namespace visualizer {
 
-CellAutomataApp::CellAutomataApp() : visualizer_(glm::vec2(Margin, Margin), CellNumRow, WindowHeight - 2 * Margin) {
+CellAutomataApp::CellAutomataApp() :
+  visualizer_(glm::vec2(Margin, Margin), CellNumRow, WindowHeight - 2 * Margin)
+{
   ci::app::setWindowSize((int) WindowHeight + 150, (int) WindowHeight);
+  // app_mode_ = game_of_life;
+  app_mode_ = forest_fire_simulator;
+  visualizer_.SetAppMode(app_mode_);
   is_runing_ = false;
+  is_paused_ = false;
   brush_color_ = 1;
   runing_step_counter_ = 10;
 }
 
 void CellAutomataApp::draw() {
-  ci::Color8u background_color(255, 255, 255);
-  ci::gl::clear(background_color);
 
-  if (is_runing_) {
-    if (runing_step_counter_ <= 0) {
-      runing_step_counter_ = 3;
-      visualizer_.Step();
+  if (app_mode_ == game_of_life) {
+    ci::Color8u background_color(255, 255, 255);
+    ci::gl::clear(background_color);
+
+    if (is_runing_) {
+      if (runing_step_counter_ <= 0) {
+        runing_step_counter_ = 3;
+        visualizer_.Step();
+      }
+      else {
+        runing_step_counter_ -= 1;
+      }
     }
-    else {
-      runing_step_counter_ -= 1;
-    }
+
+    visualizer_.Draw();
+
+    ci::gl::color(ci::Color("black"));
+    ci::gl::drawStrokedRect(ci::Rectf(glm::vec2(900, 100), glm::vec2(950, 130)));
+    ci::gl::drawStringCentered("Black", glm::vec2(925, 110), ci::Color("black"));
+
+    ci::gl::color(ci::Color("black"));
+    ci::gl::drawStrokedRect(ci::Rectf(glm::vec2(900, 150), glm::vec2(950, 180)));
+    ci::gl::drawStringCentered("White", glm::vec2(925, 160), ci::Color("black"));
+
+
+    ci::gl::color(ci::Color("black"));
+    ci::gl::drawStrokedRect(ci::Rectf(glm::vec2(900, 400), glm::vec2(950, 430)));
+    ci::gl::drawStringCentered("Run", glm::vec2(925, 410), ci::Color("black"));
+
+    ci::gl::color(ci::Color("black"));
+    ci::gl::drawStrokedRect(ci::Rectf(glm::vec2(900, 450), glm::vec2(950, 480)));
+    ci::gl::drawStringCentered("Pause", glm::vec2(925, 460), ci::Color("black"));
   }
 
-  visualizer_.Draw();
+  else if (app_mode_ == forest_fire_simulator) {
+    // std::cout << "Not implemented" << std::endl;
 
-  ci::gl::color(ci::Color("black"));
-  ci::gl::drawStrokedRect(ci::Rectf(glm::vec2(900, 100), glm::vec2(950, 130)));
-  ci::gl::drawStringCentered("Black", glm::vec2(925, 110), ci::Color("black"));
+    ci::Color8u background_color(255, 255, 255);
+    ci::gl::clear(background_color);
 
-  ci::gl::color(ci::Color("black"));
-  ci::gl::drawStrokedRect(ci::Rectf(glm::vec2(900, 150), glm::vec2(950, 180)));
-  ci::gl::drawStringCentered("White", glm::vec2(925, 160), ci::Color("black"));
+    if (is_runing_) {
+      if (runing_step_counter_ <= 0) {
+        runing_step_counter_ = 1;
+        visualizer_.Step();
+      }
+      else {
+        runing_step_counter_ -= 1;
+      }
+    }
+
+    visualizer_.Draw();
+
+    ci::gl::color(ci::Color("black"));
+    ci::gl::drawStrokedRect(ci::Rectf(glm::vec2(900, 100), glm::vec2(950, 130)));
+    ci::gl::drawStringCentered("Grassland", glm::vec2(925, 110), ci::Color("black"));
+
+    ci::gl::color(ci::Color("black"));
+    ci::gl::drawStrokedRect(ci::Rectf(glm::vec2(900, 150), glm::vec2(950, 180)));
+    ci::gl::drawStringCentered("Scrubland", glm::vec2(925, 160), ci::Color("black"));
+
+    ci::gl::color(ci::Color("black"));
+    ci::gl::drawStrokedRect(ci::Rectf(glm::vec2(900, 200), glm::vec2(950, 230)));
+    ci::gl::drawStringCentered("Forest", glm::vec2(925, 210), ci::Color("black"));
+
+    ci::gl::color(ci::Color("black"));
+    ci::gl::drawStrokedRect(ci::Rectf(glm::vec2(900, 250), glm::vec2(950, 280)));
+    ci::gl::drawStringCentered("Lake", glm::vec2(925, 260), ci::Color("black"));
+
+    ci::gl::color(ci::Color("black"));
+    ci::gl::drawStrokedRect(ci::Rectf(glm::vec2(900, 300), glm::vec2(950, 330)));
+    ci::gl::drawStringCentered("Burning", glm::vec2(925, 310), ci::Color("black"));
 
 
-  ci::gl::color(ci::Color("black"));
-  ci::gl::drawStrokedRect(ci::Rectf(glm::vec2(900, 400), glm::vec2(950, 430)));
-  ci::gl::drawStringCentered("Run", glm::vec2(925, 410), ci::Color("black"));
+    ci::gl::color(ci::Color("black"));
+    ci::gl::drawStrokedRect(ci::Rectf(glm::vec2(900, 400), glm::vec2(950, 430)));
+    ci::gl::drawStringCentered("Run", glm::vec2(925, 410), ci::Color("black"));
 
-  ci::gl::color(ci::Color("black"));
-  ci::gl::drawStrokedRect(ci::Rectf(glm::vec2(900, 450), glm::vec2(950, 480)));
-  ci::gl::drawStringCentered("Pause", glm::vec2(925, 460), ci::Color("black"));
+    ci::gl::color(ci::Color("black"));
+    ci::gl::drawStrokedRect(ci::Rectf(glm::vec2(900, 450), glm::vec2(950, 480)));
+    ci::gl::drawStringCentered("Pause", glm::vec2(925, 460), ci::Color("black"));
+
+    ci::gl::color(ci::Color("black"));
+    ci::gl::drawStrokedRect(ci::Rectf(glm::vec2(900, 500), glm::vec2(950, 530)));
+    ci::gl::drawStringCentered("Stop", glm::vec2(925, 510), ci::Color("black"));
+
+  }
+
 }
 
 void CellAutomataApp::mouseDown(ci::app::MouseEvent event) {
 
   glm::vec2 pos = event.getPos();
 
-  if (
-      pos.x > Margin && pos.x < WindowHeight - Margin && 
-      pos.y > Margin && pos.y < WindowHeight - Margin
-  ) {
-    visualizer_.HandleBrush(pos, brush_color_);
-  } else if (
-      pos.x > 900 && pos.x < 950 && 
-      pos.y > 400 && pos.y < 430
-  ) {
-    is_runing_ = 1;
-    runing_step_counter_ = 10;
-  } else if (
-      pos.x > 900 && pos.x < 950 && 
-      pos.y > 450 && pos.y < 480
-  ) {
-    is_runing_ = 0;
-    runing_step_counter_ = 10;
-  } else if (
-      pos.x > 900 && pos.x < 950 && 
-      pos.y > 100 && pos.y < 130
-  ) {
-    brush_color_ = 1;
-  } else if (
-      pos.x > 900 && pos.x < 950 && 
-      pos.y > 150 && pos.y < 180
-  ) {
-    brush_color_ = 0;
+  if (app_mode_ == game_of_life) {
+
+    if (
+        pos.x > Margin && pos.x < WindowHeight - Margin && 
+        pos.y > Margin && pos.y < WindowHeight - Margin
+    ) {
+      visualizer_.HandleBrush(pos, brush_color_);
+    } else if (
+        pos.x > 900 && pos.x < 950 && 
+        pos.y > 400 && pos.y < 430
+    ) {
+      is_runing_ = true;
+      is_paused_ = false;
+      runing_step_counter_ = 10;
+    } else if (
+        pos.x > 900 && pos.x < 950 && 
+        pos.y > 450 && pos.y < 480
+    ) {
+      is_runing_ = false;
+      is_paused_ = true;
+      runing_step_counter_ = 10;
+    } else if (
+        pos.x > 900 && pos.x < 950 && 
+        pos.y > 100 && pos.y < 130
+    ) {
+      brush_color_ = 1;
+    } else if (
+        pos.x > 900 && pos.x < 950 && 
+        pos.y > 150 && pos.y < 180
+    ) {
+      brush_color_ = 0;
+    }
   }
 
+  else if (app_mode_ == forest_fire_simulator) {
+    // std::cout << "Not implemented" << std::endl;
+
+    if (
+        pos.x > Margin && pos.x < WindowHeight - Margin && 
+        pos.y > Margin && pos.y < WindowHeight - Margin
+    ) {
+      if (!is_runing_) {
+        visualizer_.HandleBrush(pos, brush_color_);
+      }
+    } else if (
+        pos.x > 900 && pos.x < 950 && 
+        pos.y > 400 && pos.y < 430
+    ) {
+      is_runing_ = true;
+      is_paused_ = false;
+      runing_step_counter_ = 10;
+      visualizer_.BackupLandform();
+
+    } else if (
+        pos.x > 900 && pos.x < 950 && 
+        pos.y > 450 && pos.y < 480
+    ) {
+      if (is_runing_) {
+        is_runing_ = false;
+        is_paused_ = true;
+        runing_step_counter_ = 10;
+      }
+    } else if (
+        pos.x > 900 && pos.x < 950 && 
+        pos.y > 500 && pos.y < 530
+    ) {
+      if (is_paused_)
+        visualizer_.LoadLandform();
+      is_runing_ = false;
+      is_paused_ = false;
+      runing_step_counter_ = 10;
+    } else if (
+        pos.x > 900 && pos.x < 950 && 
+        pos.y > 100 && pos.y < 130
+    ) {
+      brush_color_ = grassland;
+    } else if (
+        pos.x > 900 && pos.x < 950 && 
+        pos.y > 150 && pos.y < 180
+    ) {
+      brush_color_ = scrubland;
+    } else if (
+        pos.x > 900 && pos.x < 950 && 
+        pos.y > 200 && pos.y < 230
+    ) {
+      brush_color_ = forest;
+    } else if (
+        pos.x > 900 && pos.x < 950 && 
+        pos.y > 250 && pos.y < 280
+    ) {
+      brush_color_ = lake;
+    } else if (
+        pos.x > 900 && pos.x < 950 && 
+        pos.y > 300 && pos.y < 330
+    ) {
+      brush_color_ = burning;
+    }
+
+  }
 }
 
 void CellAutomataApp::mouseDrag(ci::app::MouseEvent event) {
--- src/cell_automata_visualizer.cc
+++ src/cell_automata_visualizer.cc
@@ -1,4 +1,5 @@
 #include <cell_automata_visualizer.h>
+#include <cstdlib>
 
 namespace cellautomata {
 
@@ -6,12 +7,137 @@ namespace visualizer {
 
 using glm::vec2;
 
+
+float randf() {
+    return (float)rand() / RAND_MAX;
+}
+
+void step_rule_game_of_life(
+  std::vector<std::vector<unsigned char>> &states,
+  std::vector<std::vector<unsigned char>> &next_states
+) {
+
+  int N = states.size();
+
+  for (auto row = 0; row < N; ++row) {
+    for (auto col = 0; col < N; ++ col) {
+
+      int count_nighbors_alive = 0;
+      for (int i = -1; i < 2; i ++) {
+        for (int j = -1; j < 2; j ++) {
+          if (i == 0 && j == 0)
+            continue;
+          auto _i = (i + row + N) % N;
+          auto _j = (j + col + N) % N;
+
+          if (states[_i][_j]) {
+            count_nighbors_alive += 1;
+          }
+        }
+      }
+
+      if (states[row][col] == 1) {
+        if (count_nighbors_alive == 2 || count_nighbors_alive == 3) {
+          next_states[row][col] = 1;
+        } else {
+          next_states[row][col] = 0;
+        }
+      } else {
+        if (count_nighbors_alive == 3) {
+          next_states[row][col] = 1;
+        } else {
+          next_states[row][col] = 0;
+        }
+      }
+    }
+  }
+}
+
+void step_rule_forest_fire_simulator(
+  std::vector<std::vector<unsigned char>> &states,
+  std::vector<std::vector<unsigned char>> &landforms,
+  std::vector<std::vector<float>> &burning_timer,
+  std::vector<std::vector<unsigned char>> &next_states
+) {
+
+  int N = states.size();
+
+  for (auto row = 0; row < N; ++row) {
+    for (auto col = 0; col < N; ++ col) {
+
+      int count_nighbors_burning = 0;
+      float count_nighbors_burning_time = 0.0f;
+
+      for (int i = -1; i < 2; i ++) {
+        for (int j = -1; j < 2; j ++) {
+          if (i == 0 && j == 0)
+            continue;
+          auto _i = i + row;
+          auto _j = j + col;
+
+          if (_i < 0 || _j < 0)
+            continue;
+          if (_i >= N || _j >= N)
+            continue;
+
+          if (states[_i][_j] == burning) {
+            count_nighbors_burning += 1;
+            if (abs(i) + abs(j) == 1)
+              count_nighbors_burning_time += burning_timer[_i][_j];
+            else
+              count_nighbors_burning_time += burning_timer[_i][_j] * 0.7f;
+          }
+        }
+      }
+
+      int burn_time[5] = {1, 5, 12, -1, 3};
+      int burnout_time[5] = {2, 15, 36, -1, 4};
+
+      // burning to burnout
+      if (states[row][col] == burning) {
+        
+        int _burnout_time = burnout_time[landforms[row][col]];
+
+        if (burning_timer[row][col] > _burnout_time && randf() > 0.7) {
+          next_states[row][col] = burnout;
+          burning_timer[row][col] = 0.0f;
+        }
+        else {
+          next_states[row][col] = burning;
+          burning_timer[row][col] += 1.0f;
+        }
+
+      }
+
+      // burnout stay burnout
+      else if (states[row][col] == burnout) {
+        next_states[row][col] = burnout;
+      }
+
+      // notburning to burning
+      else {
+
+        int _burn_time = burn_time[landforms[row][col]];
+
+        if (_burn_time != -1 && count_nighbors_burning_time > _burn_time && randf() > 0.2) {
+          next_states[row][col] = burning;
+          burning_timer[row][col] = 0.0f;
+        }
+        else {
+          next_states[row][col] = states[row][col];
+        }
+      }
+
+    }
+  }
+}
+
+
 CellAutomataVisualizer::CellAutomataVisualizer(
   const vec2& top_left_corner,
   int num_cells_row,
   double visualizer_size
 ) {
-
     top_left_ = top_left_corner;
     num_cells_row_ = num_cells_row;
     cell_width_ = visualizer_size / num_cells_row;
@@ -19,24 +145,48 @@ CellAutomataVisualizer::CellAutomataVisualizer(
     for (auto i = 0; i < num_cells_row_; i++) {
       std::vector<unsigned char> row;
       std::vector<unsigned char> row_temp;
+      std::vector<unsigned char> row_land;
+      std::vector<float> row_burning_timer;
       for (auto j = 0; j < num_cells_row_; j++) {
         row.push_back(0);
         row_temp.push_back(0);
+        row_land.push_back(0);
+        row_burning_timer.push_back(0.0f);
       }
       CA_state_.push_back(row);
       CA_state_temp_.push_back(row_temp);
+      CA_landforms_.push_back(row_land);
+      CA_burning_timer_.push_back(row_burning_timer);
     }
-    ;
 }
 
 void CellAutomataVisualizer::Draw() const {
   for (auto row = 0; row < num_cells_row_; ++row) {
     for (auto col = 0; col < num_cells_row_; ++col) {
-      if (CA_state_[row][col] == 1) {
-        ci::gl::color(ci::Color::gray(0.3f));
+
+      if (app_mode_ == game_of_life) {
+        if (CA_state_[row][col] == 1) {
+          ci::gl::color(ci::Color::gray(0.3f));
+        }
+        else {
+          ci::gl::color(ci::Color("white"));
+        }
       }
-      else {
-        ci::gl::color(ci::Color("white"));
+
+      else if (app_mode_ == forest_fire_simulator) {
+        if (CA_state_[row][col] == grassland) {
+          ci::gl::color(ci::Color8u(154, 242, 170));
+        } else if (CA_state_[row][col] == scrubland) {
+          ci::gl::color(ci::Color8u(72, 204, 97));
+        } else if (CA_state_[row][col] == forest) {
+          ci::gl::color(ci::Color8u(31, 113, 47));
+        } else if (CA_state_[row][col] == lake) {
+          ci::gl::color(ci::Color8u(72, 152, 232));
+        } else if (CA_state_[row][col] == burning) {
+          ci::gl::color(ci::Color8u(229, 49, 49));
+        } else if (CA_state_[row][col] == burnout) {
+          ci::gl::color(ci::Color8u(150, 150, 150));
+        }
       }
 
       vec2 cell_top_left = top_left_ + vec2(col * cell_width_, row * cell_width_);
@@ -62,7 +212,7 @@ void CellAutomataVisualizer::HandleBrush(const vec2& brush_screen_coords, unsign
       vec2 cell_center = {col + 0.5, row + 0.5};
 
       if (glm::distance(brush_vis_coords, cell_center) <=
-          2.0f) {
+          5.0f) {
         CA_state_[row][col] = (unsigned char) color;
       }
     }
@@ -80,36 +230,11 @@ void CellAutomataVisualizer::Clear() {
 
 void CellAutomataVisualizer::Step() {
 
-  for (auto row = 0; row < num_cells_row_; ++row) {
-    for (auto col = 0; col < num_cells_row_; ++ col) {
-
-      int count_nighbors_alive = 0;
-      for (int i = -1; i < 2; i ++) {
-        for (int j = -1; j < 2; j ++) {
-          if (i == 0 && j == 0)
-            continue;
-          auto _i = (i + row) % num_cells_row_;
-          auto _j = (j + col) % num_cells_row_;
-          if (CA_state_[_i][_j]) {
-            count_nighbors_alive += 1;
-          }
-        }
-      }
-
-      if (CA_state_[row][col] == 1) {
-        if (count_nighbors_alive == 2 || count_nighbors_alive == 3) {
-          CA_state_temp_[row][col] = 1;
-        } else {
-          CA_state_temp_[row][col] = 0;
-        }
-      } else {
-        if (count_nighbors_alive == 3) {
-          CA_state_temp_[row][col] = 1;
-        } else {
-          CA_state_temp_[row][col] = 0;
-        }
-      }
-    }
+  if (app_mode_ == game_of_life) {
+    step_rule_game_of_life(CA_state_, CA_state_temp_);
+  }
+  else if (app_mode_ == forest_fire_simulator) {
+    step_rule_forest_fire_simulator(CA_state_, CA_landforms_, CA_burning_timer_, CA_state_temp_);
   }
 
   for (auto row = 0; row < num_cells_row_; ++row) {
@@ -119,6 +244,27 @@ void CellAutomataVisualizer::Step() {
   }
 
 }
+
+void CellAutomataVisualizer::SetAppMode(app_t app_mode) {
+  app_mode_ = app_mode;
+}
+
+void CellAutomataVisualizer::BackupLandform() {
+  for (auto row = 0; row < num_cells_row_; ++row) {
+    for (auto col = 0; col < num_cells_row_; ++col) {
+      CA_landforms_[row][col] = CA_state_[row][col];
+    }
+  }
+}
+
+void CellAutomataVisualizer::LoadLandform() {
+  for (auto row = 0; row < num_cells_row_; ++row) {
+    for (auto col = 0; col < num_cells_row_; ++col) {
+      CA_state_[row][col] = CA_landforms_[row][col];
+    }
+  }
+}
+
 
 }  // namespace visualizer
 
